afca2055f1daaf89da05ae418f899fe59362b730
Buddy Lindsey, Jr. <buddy@buddylindsey.com>
2025-08-26 22:04:52 -0500

    Update asdf

--- ubuntu/asdf.sh
+++ ubuntu/asdf.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 # Source utils in parent
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-source "$SCRIPT_DIR/../utils.sh"
 
-check_command_installed "asdf"
-if [ $? -eq 0 ]; then
-    exit 0
+VERBOSE="${VERBOSE:-no}"
+
+pushd /tmp
+
+REPO='asdf-vm/asdf'
+APP_VERSION=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+ARCHIVE_FILE='asdf.tar.gz'
+DOWNLOAD_ARCHIVE="asdf-$APP_VERSION-linux-amd64.tar.gz"
+APP_DIR='/usr/local/bin'
+EXTRACT_LOCATION='asdf'
+
+if [[ $VERBOSE == "yes" ]]; then
+    echo "REPO: $REPO"
+    echo "VERSION: $APP_VERSION"
+    echo "DOWNLOAD_ARCHIVE: $DOWNLOAD_ARCHIVE"
 fi
 
-sudo apt install -y curl git build-essential libssl-dev libffi-dev liblzma-dev
-ASDF_VERSION=$(curl -s "https://api.github.com/repos/asdf-vm/asdf/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v$ASDF_VERSION
+curl -sLo $ARCHIVE_FILE https://github.com/$REPO/releases/download/$APP_VERSION/$DOWNLOAD_ARCHIVE
+tar -xzvf $ARCHIVE_FILE
+sudo install $EXTRACT_LOCATION $APP_DIR
+popd
 
-export ASDF_DIR=$HOME/.asdf
-source "$ASDF_DIR/asdf.sh"
-~/.asdf/bin/asdf plugin add python
-~/.asdf/bin/asdf install python latest
-~/.asdf/bin/asdf global python latest
-~/.asdf/bin/asdf plugin add nodejs 
-~/.asdf/bin/asdf install nodejs latest
-~/.asdf/bin/asdf global nodejs latest
+asdf plugin add python
+asdf install python latest
+asdf global python latest
+asdf plugin add nodejs
+asdf install nodejs latest
+asdf global nodejs latest
+asdf plugin add golang
+asdf install golang latest
